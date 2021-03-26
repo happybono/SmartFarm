197d111082b73ae9fa42859d72a4ba41c1eabcca
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2021-03-26 10:57:28 +0900

    Create MySSID.h

---
+++ MySSID.h
@@ -0,0 +1,10 @@
+#define mySSID1    "Galaxy_NOTE_10+"
+#define myPASS1    "..."
+
+#define mySSID2    "SK_WiFiGIGA5AA4_5G"
+#define myPASS2    "..."
+
+#define mySSID3    "iPhone_wifi"
+#define myPASS3    "12345678"
+
+#define thingSpeak "OR5..."
